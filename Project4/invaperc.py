4740d9e623b88f6fd839044c5db23a95dc5839cc
atomes <sajjadah@uio.no>
2019-04-05 15:43:30 +0200

    done

--- Project4/invaperc.py
+++ Project4/invaperc.py
@@ -3,47 +3,68 @@ from scipy.ndimage import measurements
 
 
 L = 50 # system size
-p = linspace(0,0.7,71)
-perc = 0 # flag to signal if other end is reached
-nbetween = 1
-nstep = 0
-nend = len(p)
-nstop = 0
-z = rand(L,L)  # Random distribution of thresholds 
-pcluster = zeros((L,L)) 
-while ((nstop==0) and (nstep<nend)): 
-	nstep = nstep + 1 
-	p0 = p[nstep] 
-	zz = z<p0 
-	[lw,num] = measurements.label(zz) 
-	leftside = lw[:,0] 
-	il = argwhere(leftside>0)
-	leftnonzero = leftside[il]  
-	#print leftside
-	uniqueleftside = unique(leftnonzero) 
-	#print uniqueleftside
-	cluster = isin(lw, uniqueleftside) #ismember(lw,uniqueleftside)
-	print cluster
-	pcluster = pcluster + cluster.astype(float) 
-	#print pcluster
-	
-	if (nstep % nbetween==0):
-		imshow(pcluster[pcluster>0])
-		colorbar()
-		draw()
-		pause(.01)
-		close()
-	
-	# Check if it has reached the right hand side
-	rightside = lw[:,-1] 
-	ir = argwhere(rightside>0) 
-	rightnonzero = rightside[ir] 
-	span = intersect1d(leftnonzero,rightnonzero)
-	if len(span)>0: 
-		nstop = 1  # spanning end
+p = linspace(0, 0.7, 8)
+
+
+M = 1000
+Ns = zeros((len(p), M))
+
+for mi in range(M):
+	z = rand(L,L)  # Random distribution of thresholds 
+	pcluster = zeros((L,L))
+
+	perc = 0 # flag to signal if other end is reached
+	nbetween = 10
+	nstep = 0
+	nend = len(p)
+	nstop = 0
+
+	plot_invasion = False 
+
+	while ((nstop==0) and (nstep<nend)): 
+		nstep = nstep + 1 
+		p0 = p[nstep] 
+		zz = z < p0 
+		[lw,num] = measurements.label(zz) 
+		leftside = lw[:,0] 
+		il = argwhere(leftside>0)
+		leftnonzero = leftside[il]  
+		#print leftside
+		uniqueleftside = unique(leftnonzero) 
+		#print uniqueleftside
+		cluster = isin(lw, uniqueleftside) #ismember(lw,uniqueleftside)
+		#print cluster 
+		pcluster = pcluster + cluster.astype(float)
+		Ns[nstep - 1, mi] = len(cluster[cluster == True])
+		#print pcluster
+		
+		if (nstep % nbetween==0) and plot_invasion:
+			imshow(pcluster)
+			colorbar()
+			draw()
+			pause(.01)
+			show()
+			close()
+		
+		# Check if it has reached the right hand side
+		rightside = lw[:,-1] 
+		ir = argwhere(rightside>0) 
+		rightnonzero = rightside[ir] 
+		span = intersect1d(leftnonzero,rightnonzero)
+		if len(span)>0: 
+			nstop = 1  # spanning end
 
 
 print p0
-im = imshow(pcluster[pcluster > 0], animated=True)
-colorbar()
-show()
+if plot_invasion:
+	im = imshow(pcluster)
+	colorbar()
+	draw()
+	show()
+
+pis = [1, 2, 3]
+
+for pi in pis:
+	hist(Ns[pi, :])
+	title(p[pi])
+	show()
